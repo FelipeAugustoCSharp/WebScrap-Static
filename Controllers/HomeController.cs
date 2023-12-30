f736a950143751f0b72e63dde72eecc386bce559
Kammai <felipeaugustobonfimdealmeida@gmail.com>
2023-12-30 20:32:44 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebScrap.Models;
+using HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+
+
+
+namespace WebScrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+       /* public IActionResult Index()
+        {
+            return View();
+        }*/
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        private static async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync(fullUrl);
+            return response;
+        }
+
+        public IActionResult Index()
+        {
+            string url = "https://en.wikipedia.org/wiki/List_of_programmers";
+            string response = CallUrl(url).Result;
+
+            var linkList = ParseHtml(response);
+
+            WriteToCsv(linkList);
+
+            return View();
+        }
+
+        private List<string> ParseHtml(string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var programmerLinks = htmlDoc.DocumentNode.SelectNodes("//li[not(contains(@class, 'tocsection'))]");
+            /*var programmerLinks = htmlDoc.DocumentNode.Descendants("li")
+                .Where(node => !node.GetAttributeValue("class", "").Contains("tocsection"))
+                .ToList();*/
+
+            List<string> wikiLink = new List<string>();
+
+            foreach (var link in programmerLinks)
+            {
+                if (link.FirstChild.Attributes.Count > 0)
+                {
+                    wikiLink.Add("https://en.wikipedia.org/" + link.FirstChild.Attributes[0].Value);
+                }
+            }
+
+            return wikiLink;
+        }
+
+        private void WriteToCsv(List<string> links)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var link in links)
+            {
+                sb.AppendLine(link);
+            }
+
+            System.IO.File.WriteAllText("links.csv", sb.ToString());
+        }
+    }
+}
